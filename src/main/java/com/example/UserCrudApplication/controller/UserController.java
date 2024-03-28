55ab72adac8bb84db8734d316ceab9139ae61eef
Manjul Tamang <manzultamang777@gmail.com>
2024-03-28 16:01:33 +0545

    create new user

--- src/main/java/com/example/UserCrudApplication/controller/UserController.java
+++ src/main/java/com/example/UserCrudApplication/controller/UserController.java
@@ -1,10 +1,25 @@
 package com.example.UserCrudApplication.controller;
 
+import com.example.UserCrudApplication.controller.request.UserRequest;
+import com.example.UserCrudApplication.model.User;
+import com.example.UserCrudApplication.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/users")
+    public User createNewUser(@Valid @RequestBody UserRequest userRequest){
+        return userService.createUser(userRequest);
+
+    }
 
 }
--- src/main/java/com/example/UserCrudApplication/service/UserService.java
+++ src/main/java/com/example/UserCrudApplication/service/UserService.java
@@ -1,4 +1,22 @@
 package com.example.UserCrudApplication.service;
 
+import com.example.UserCrudApplication.controller.request.UserRequest;
+import com.example.UserCrudApplication.model.User;
+import com.example.UserCrudApplication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public User createUser(UserRequest userRequest) {
+        User user = new User();
+        user.setUserName(userRequest.getUserName());
+        user.setUserAddress(userRequest.getUserAddress());
+        user.setUserEmail(userRequest.getUserEmail());
+        return userRepository.save(user);
+    }
+
 }
