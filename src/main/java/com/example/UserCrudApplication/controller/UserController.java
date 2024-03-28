c741f5e18217c6e79cd725d02e1a70e9dc0a05f9
Manjul Tamang <manzultamang777@gmail.com>
2024-03-28 15:46:58 +0545

    user request configured

--- src/main/java/com/example/UserCrudApplication/controller/UserController.java
+++ src/main/java/com/example/UserCrudApplication/controller/UserController.java
@@ -1,4 +1,10 @@
 package com.example.UserCrudApplication.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class UserController {
+
 }
--- src/main/java/com/example/UserCrudApplication/controller/request/UserRequest.java
+++ src/main/java/com/example/UserCrudApplication/controller/request/UserRequest.java
@@ -1,4 +1,50 @@
 package com.example.UserCrudApplication.controller.request;
 
+import jakarta.persistence.Column;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 public class UserRequest {
+
+    @Valid
+    @NotEmpty
+    private String userName;
+    @NotNull
+    @Email
+    private String userEmail;
+    @NotBlank
+    private String userAddress;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public UserRequest(String userName, String userEmail, String userAddress) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userAddress = userAddress;
+    }
 }
