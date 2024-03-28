1a29312f629ce9a778e6ecee428f9de5465d8137
Manjul Tamang <manzultamang777@gmail.com>
2024-03-28 15:37:17 +0545

    user model addded

--- src/main/java/com/example/UserCrudApplication/model/User.java
+++ src/main/java/com/example/UserCrudApplication/model/User.java
@@ -1,4 +1,69 @@
 package com.example.UserCrudApplication.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    @Column(name="username")
+    private String userName;
+    @Column(name="email")
+    private String userEmail;
+    @Column(name="address")
+    private String userAddress;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public User(Long userId, String userName, String userEmail, String userAddress) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userAddress = userAddress;
+    }
+    public User(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                ", userAddress='" + userAddress + '\'' +
+                '}';
+    }
 }
